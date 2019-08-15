25f3274464c6711141c76e04104d4ef1804016c5
pparker <pparker@localhost.localdomain>
2019-08-14 21:22:09 -0400

    commit

---
+++ actions/ios_xe_varibales.py
@@ -0,0 +1,8 @@
+
+#------------------------------------------------#
+# Below are the variables for the ROM-MON Upgrade
+#------------------------------------------------#
+        
+hostip = "192.168.200.30"
+ios_image = "isr4300-universalk9.16.09.03.SPA.bin"
+md5_checksum = "sderd43r345c34ds43s23"
---
+++ actions/rom_mon_varibales.py
@@ -0,0 +1,8 @@
+
+#------------------------------------------------#
+# Below are the variables for the ROM-MON Upgrade
+#------------------------------------------------#
+        
+hostip = "192.168.200.30"
+rom_image = "isr4200_4300_rommon_169_1r_SPA.pkg"
+md5_checksum = "123456"
